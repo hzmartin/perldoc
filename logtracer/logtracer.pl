071ad21b932b0b8af5df6ea2a6e3c35cce4cda4c
hzwangqh <hzwangqihui@gmail.com>
2016-01-28 11:54:29 +0800

    log

--- logtracer/logtracer.pl
+++ logtracer/logtracer.pl
@@ -4,11 +4,10 @@ use strict;
 use lib './lib';
 
 use LogItem;
-use DateTime;
 
 if(@ARGV != 2){
-	print "usage: ./logtracer.pl openid keyword\n";
-	exit;
+    print "usage: ./logtracer.pl openid keyword\n";
+    exit;
 }
 my ($openid, $keyword) = @ARGV;
 print "openid=$openid, keyword=$keyword\n";
@@ -21,33 +20,23 @@ my $TARGET_LOG_PREFIX = "***";
 
 while(<STDIN>){
   if(/(^\d{4})-(\d{2})-(\d{2})\s(\d{2}):(\d{2}):(\d{2}),(\d{3})\s-\s.+\s\[.+-(.{13}).+\$(\d+)\]\s([INFO|ERROR|FATAL|DEBUG|WARN]+)\s(.+)/){
-	my $dt1 = DateTime->new(
-   	     year => $1,
-   	     month => $2,
-   	     day => $3,
-   	     hour => $4,
-   	     minute => $5,
-   	     second => $6,
-   	     nanosecond => $7
-	  );
-         my $log_time_str = "$1-$2-$3 $4:$5:$6,$7";
+         my $log_time = "$1-$2-$3 $4:$5:$6,$7";
          my $host_ip = $8;
          my $thread_id = $9;
          my $log_level = $10;
          my $log_detail = $_;
          my $log_item = LogItem->new ({
-            log_time => $dt1,
-            log_time_str => $log_time_str,
+            log_time => $log_time,
             host_ip => $host_ip,
             thread_id => $thread_id,
- 	    log_level => $log_level,
+         log_level => $log_level,
             log_detail => $log_detail
          });
         if($log_detail =~ $openid){
            if($log_detail =~ $keyword){
                 $log_item->{log_detail} = $TARGET_LOG_PREFIX.$log_item->{log_detail};
                 push @target_log_list, $log_item;
-	        next;
+            next;
            }
         }
         #user log map create
@@ -59,15 +48,15 @@ while(<STDIN>){
         #thread log map create
         my $thread_log = $thread_log_map{$host_ip."_".$thread_id};
         if(defined($thread_log)){
-	   push(@{$thread_log},$log_item);	
-	}else{
-	   $thread_log=[$log_item];
-	   $thread_log_map{$host_ip."_".$thread_id} = $thread_log;
-	}
+       push(@{$thread_log},$log_item);    
+    }else{
+       $thread_log=[$log_item];
+       $thread_log_map{$host_ip."_".$thread_id} = $thread_log;
+    }
         #end thread log map create
 
   }else{
-	print "in else: $_ ";
+    print "in else: $_ ";
   }
 }
 
@@ -88,7 +77,7 @@ foreach(@target_log_list){
   my $target_host_ip = $_->{host_ip};
   my $target_thread_id = $_->{thread_id};
   my $cur_time = $_->{log_time};
-  if(DateTime->compare($end_time, $cur_time)==-1){
+  if($end_time le $cur_time){
      $end_time = $cur_time;
   }
   my $exist_thread = 0;
@@ -99,13 +88,13 @@ foreach(@target_log_list){
   }
   
   if($exist_thread == 0){
-        #print "push thread log ".$target_host_ip."_".$target_thread_id."\n";
-  	push @unique_thread, $target_host_ip."_".$target_thread_id;
-        my @thread_log = @{$thread_log_map{$target_host_ip."_".$target_thread_id}};
-        push @all_log, @thread_log;
-        #foreach(@thread_log){
-        #   print "TTT".$_->{log_detail}."\n";
-        #}
+      #print "push thread log ".$target_host_ip."_".$target_thread_id."\n";
+      push @unique_thread, $target_host_ip."_".$target_thread_id;
+      my @thread_log = @{$thread_log_map{$target_host_ip."_".$target_thread_id}};
+      push @all_log, @thread_log;
+      #foreach(@thread_log){
+      #   print "TTT".$_->{log_detail}."\n";
+      #}
   }else{
         #print "skip thread log ".$target_host_ip."_".$target_thread_id."\n";
   }
@@ -116,9 +105,9 @@ push @all_log, @user_log_list;
 #   print "UUU".$_->{log_detail}."\n";
 #}
 
-#print "END_TIME: $end_time\n";
-#print "===================\n";
-foreach(sort {$a->{log_time_str} cmp $b->{log_time_str}} @all_log){
+print "END_TIME: $end_time\n";
+print "===================\n";
+foreach(sort {$a->{log_time} cmp $b->{log_time}} @all_log){
      if(DateTime->compare($end_time, $_->{log_time})==-1){
           #print "skip: $_->{log_detail}\n";
           next;
